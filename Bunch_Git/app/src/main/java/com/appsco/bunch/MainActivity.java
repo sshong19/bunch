ecb3d22f84cfe4bd58dede5bbaf612fa132dbfec
ellenlowing <lowing@bu.edu>
2016-12-12 11:15:44 -0500

    Updated MainActivity.java for firebase initializeApp func

--- Bunch_Git/app/src/main/java/com/appsco/bunch/MainActivity.java
+++ Bunch_Git/app/src/main/java/com/appsco/bunch/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        FacebookSdk.sdkInitialize(getApplicationContext());
+        FirebaseApp.initializeApp(this);
         setContentView(R.layout.activity_main);
         firebaseAuth = FirebaseAuth.getInstance();
 
